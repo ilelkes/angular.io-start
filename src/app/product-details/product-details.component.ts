f714f3eb29011d8f56acb74874cc8d3928512bf9
Lelkes István <47304380+ilelkes@users.noreply.github.com>
2021-05-07 18:47:38 +0200

    Display the cart items

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -10,6 +12,11 @@ import { products } from '../products';
 
 export class ProductDetailsComponent {
   product;
+
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
 
   ngOnInit() {
     // First get the product id from the current route.
@@ -22,6 +29,7 @@ export class ProductDetailsComponent {
 
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService
   ) { }
 
 }
